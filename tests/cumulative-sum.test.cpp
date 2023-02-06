226f5884bf2722003143c40cf74d3c9480e54466
RamSeiya <tonasho2140@gmail.com>
2023-02-06 22:47:00 +0900

    Let's GO

--- tests/cumulative-sum.test.cpp
+++ tests/cumulative-sum.test.cpp
@@ -1,6 +1,6 @@
 #define PROBLEM "https://judge.yosupo.jp/problem/static_range_sum"
-#include "../../template/template.hpp"
-#include "../../math/cumulative-sum.hpp"
+#include "template/template-yosupo-hpp"
+#include "math/cumulative-sum.hpp"
 
 int main() {
     int n, q; cin >> n >> q;
