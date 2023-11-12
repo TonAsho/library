c3c38f1edb58df61ac65b85eb7b75100866e2311
RamSeiya <tonasho2140@gmail.com>
2023-11-12 14:41:26 +0900

    URLミスった

--- tests/yosupo/unionfind.test.cpp
+++ tests/yosupo/unionfind.test.cpp
@@ -1,4 +1,4 @@
-#define PROBLEM https://judge.yosupo.jp/problem/unionfind
+#define PROBLEM "https://judge.yosupo.jp/problem/unionfind"
 #include "template/template.hpp"
 #include "datastructure/UnionFind.hpp"
 
