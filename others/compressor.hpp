5b96a8a413d9f11f8b309fd748e27bead66d7015
RamSeiya <tonasho2140@gmail.com>
2023-12-14 15:11:54 +0900

    update Compressor

--- others/compressor.hpp
+++ others/compressor.hpp
@@ -1,18 +1,31 @@
 #pragma once
 #include <bits/stdc++.h>
 
-template<typename T>
+template<typename T, typename Comp = std::less<T>>
 struct Compressor {
   private:
     std::vector<T> data;
+    Comp cmp;
     bool sorted = false;
   public:
-    Compressor(const std::vector<T> &v) : data(std::move(v)) {}
+    Compressor() : Compressor(Comp()) {}
+    Compressor(const Comp &cmp) : cmp(cmp) {}
+    Compressor(const std::vector<T> &v, const Comp &cmp = Comp()) : data(v), cmp(cmp) {}
+    Compressor(std::vector<T> &&v, const Comp &cmp = Comp()) : data(std::move(v)), cmp(cmp) {}
+    Compressor(std::initializer_list<T> li, const Comp &cmp = Comp()) : data(li.begin(), li.end()), cmp(cmp) {}
+    void push_back(const T &v) { assert(!sorted); data.push_back(v); }
+    void push_back(T &&v) { assert(!sorted); data.push_back(std::move(v)); }
+    void push(const std::vector<T> &v) {
+        assert(!sorted);
+        const int n = data.size();
+        data.resize(v.size() + n);
+        for(int i = 0; i < (int)v.size(); i++) data[i + n] = v[i];
+    }
     void build() {
         assert(!sorted);
         sorted = 1;
-        std::sort(data.begin(), data.end());
-        data.erase(std::unique(data.begin(), data.end()), data.end());
+        std::sort(data.begin(), data.end(), cmp);
+        data.erase(std::unique(data.begin(), data.end(), [&](const T &l, const T &r) { return !cmp(l, r) && !cmp(r, l); }), data.end());
     }
     int get_index(const T &v) {
         assert(sorted);
@@ -29,6 +42,7 @@ struct Compressor {
         return res;
     }
     int size() const {
+        assert(sorted);
         return (int)data.size();
     }
 };
--- others/monoid.hpp
+++ others/monoid.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include <bits/stdc++.h>
+#include "template/alias.hpp"
 
 namespace Monoid {
     template<typename T>
